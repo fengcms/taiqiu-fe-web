c8b10c94100e983190129c8f2a3ef0b5c8790bc7
fungleo <web@fengcms.com>
2018-12-17 18:08:46 +0800

    修改：     src/views/Home.vue

--- src/coms/myHeader.vue
+++ src/coms/myHeader.vue
@@ -3,7 +3,7 @@
     <div class="web">
       <h1 id="logo" class="logo">
         <img src="@/assets/logo.svg" alt="">
-        <a href="/">程序员的每日清台</a>
+        <a href="/">程序员的一杆清台</a>
       </h1>
       <nav class="nav">
         <ul>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,28 @@
     <myHeader></myHeader>
     <myBanner></myBanner>
     <subTitle title="程序员的一杆清台" tip="看高手清台学不会？那就学普通人是怎么台球一杆清台的！"></subTitle>
+    <div class="home_tab">
+      <template v-for="(i, k) in catalog">
+        <span v-if="i.type === 'video'" :key="k" :class="`${tabActive === k ? 'active' : ''}`" @click="changeTab(k)">{{i.name}}</span>
+      </template>
+    </div>
+    <div class="home_video">
+      <ul v-for="(i, k) in homeVideo" :key="k" v-show="k === tabActive">
+        <div v-if="i.children.length === 0" class="home_video_nothing"></div>
+        <template v-else>
+          <li v-for="(ii, kk) in i.children" :key="k + '' + kk">
+            <router-link to="#">
+              <div class="home_video_cover">
+                <i :style="`background-image: url(${ii.cover});`">
+                  <img :src="ii.cover" :alt="ii.title" />
+                </i>
+              </div>
+              <span>{{ii.title}}</span>
+            </router-link>
+          </li>
+        </template>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -14,17 +36,26 @@ export default {
   components: { myHeader, myBanner, subTitle },
   data () {
     return {
+      catalog: [],
+      tabActive: 0,
+      homeVideo: []
     }
   },
   created () {
-    // this.getData()
+    this.getData()
   },
   methods: {
     getData () {
       this.$api.get('catalog', null, r => {
+        this.catalog = r.data.list
+      })
+      this.$api.get('home-video', null, r => {
+        this.homeVideo = r.data.list
         console.log(r)
       })
-      console.log('get data')
+    },
+    changeTab (index) {
+      this.tabActive = index
     }
   }
 }
